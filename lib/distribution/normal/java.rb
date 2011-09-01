76f2385a1ebe36f897e5f95a3a8c04009c2561cf
Matthew Peterson <matt@mpeterson.org>
2011-08-31 21:44:23 -0400

    Java Normal distribution documented

--- lib/distribution/normal/java.rb
+++ lib/distribution/normal/java.rb
@@ -8,21 +8,29 @@ module Distribution
     # TODO
     module Java_
       class << self
+        #==
+        # Generate random variables from the provided distribution
         def rng(mean=0,sigma=1,seed=nil)
           dist = NormalDistributionImpl.new(mean, sigma)
           lambda { dist.sample }
         end
         
+        #==
+        # Get the inverse cumulative density function (p-value) for qn
         def p_value(qn)
           dist = NormalDistributionImpl.new
           dist.inverseCumulativeProbability(qn)
         end
         
+        #==
+        # Return the cumulative density function at x
         def cdf(x)
           dist = NormalDistributionImpl.new
           dist.cumulativeProbability(x)
         end
         
+        #==
+        # Return the probability density function at x
         def pdf(x)
           dist = NormalDistributionImpl.new
           dist.density(x)
