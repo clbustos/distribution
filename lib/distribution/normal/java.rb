ed6bd348556d3fd89f7b1a68a4f159a3134880ad
Matthew Peterson <matt@mpeterson.org>
2011-08-31 21:42:08 -0400

    Normal distribution in JRuby

--- lib/distribution/normal/java.rb
+++ lib/distribution/normal/java.rb
@@ -1,8 +1,33 @@
+$:.unshift(File.dirname(__FILE__)+"/../../../java_lib")
+require 'java'
+require 'commons-math-2.2.jar'
+java_import org.apache.commons.math.distribution.NormalDistributionImpl
+
 module Distribution
   module Normal
     # TODO
     module Java_
       class << self
+        def rng(mean=0,sigma=1,seed=nil)
+          dist = NormalDistributionImpl.new(mean, sigma)
+          lambda { dist.sample }
+        end
+        
+        def p_value(qn)
+          dist = NormalDistributionImpl.new
+          dist.inverseCumulativeProbability(qn)
+        end
+        
+        def cdf(x)
+          dist = NormalDistributionImpl.new
+          dist.cumulativeProbability(x)
+        end
+        
+        def pdf(x)
+          dist = NormalDistributionImpl.new
+          dist.density(x)
+        end
+      
       end
     end
   end
