dc62e30fe5d40ef55a9decda76660b9124d908f4
dennyabraham <email@dennyabraham.com>
2011-11-15 00:30:23 -0500

    add lognormal distribution
     * caveat, no tests yet

---
+++ lib/distribution/lognormal.rb
@@ -0,0 +1,36 @@
+require 'distribution/lognormal/gsl'
+
+module Distribution
+  # From Wikipedia:
+  #    In probability theory, a log-normal distribution is a probability
+  #    distribution of a random variable whose logarithm is normally
+  #    distributed. If X is a random variable with a normal distribution, then
+  #    Y = exp(X) has a log-normal distribution; likewise, if Y is
+  #    log-normally distributed, then X = log(Y) is normally distributed. (This
+  #    is true regardless of the base of the logarithmic function: if loga(Y) is
+  #    normally distributed, then so is logb(Y), for any two positive numbers
+  #    a, b ≠ 1.)
+  #
+  # This module calculate the pdf, cdf and inverse cdf for Beta Distribution.
+  #
+  module Lognormal
+    extend Distributable
+    SHORTHAND='lognormal'
+    create_distribution_methods
+
+    ##
+    # :singleton-method: pdf(x,z,s)
+    # Returns PDF of of Lognormal distribution with parameters z and s
+
+
+    ##
+    # :singleton-method: cdf(x,z,s)
+    # Returns the CDF of Lognormal distribution of x with parameters z and s
+
+    ##
+    # :singleton-method: p_value(pr,z,s)
+    # Return the quantile of the corresponding integral +pr+
+    # on a lognormal distribution's cdf with parameters z and s
+
+  end
+end
