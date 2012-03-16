b26645f81810fe46a533ebf4128727763c3239c8
Francisco Gutierrez <freefrancisco@gmail.com>
2012-03-15 19:00:03 -0700

    fixed problem with gem breaking integer division in ruby 1.8.7 by getting rid of mathn

--- lib/distribution/math_extension.rb
+++ lib/distribution/math_extension.rb
@@ -11,7 +11,46 @@ require "distribution/math_extension/log_utilities"
 
 
 if RUBY_VERSION<"1.9"
-  require 'mathn'
+  class Prime
+    include Enumerable
+
+    def initialize
+      @seed = 1
+      @primes = []
+      @counts = []
+    end
+
+    def succ
+      i = -1
+      size = @primes.size
+      while i < size
+        if i == -1
+    @seed += 1
+    i += 1
+        else
+    while @seed > @counts[i]
+      @counts[i] += @primes[i]
+    end
+    if @seed != @counts[i]
+      i += 1
+    else
+      i = -1
+    end
+        end
+      end
+      @primes.push @seed
+      @counts.push @seed + @seed
+      return @seed
+    end
+    alias next succ
+
+    def each
+      loop do
+        yield succ
+      end
+    end
+  end
+
   def Prime.each(upper,&block) 
     @primes=Prime.new
     @primes.each do |prime|
