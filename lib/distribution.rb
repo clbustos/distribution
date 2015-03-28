ebc3c53137b75dd777ea3144ddf95a6930cd5bd0
Carlos Agarie <carlos.agarie@gmail.com>
2015-03-28 00:07:33 -0300

    Refactor Distribution::Shorthand::add_shortcut

--- lib/distribution.rb
+++ lib/distribution.rb
@@ -46,12 +46,13 @@ module Distribution
     EQUIVALENCES = { p_value: :p, cdf: :cdf, pdf: :pdf, rng: :r,
                      exact_pdf: :epdf, exact_cdf: :ecdf, exact_p_value: :ep }
 
-    def self.add_shortcut(sh, m, &block)
-      if EQUIVALENCES.include? m.to_sym
-        sh_name = sh + "_#{m}"
-        define_method(sh_name, &block)
-        sh_name = sh + "_#{EQUIVALENCES[m.to_sym]}"
-        define_method(sh_name, &block)
+    def self.add_shortcut(shortcut, method, &block)
+      if EQUIVALENCES.include? method.to_sym
+        name = shortcut + "_#{method}"
+        define_method(name, &block)
+
+        name = shortcut + "_#{EQUIVALENCES[method.to_sym]}"
+        define_method(name, &block)
 
       end
     end
