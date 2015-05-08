1135e835e96b3300f89488b4855aeb6d9bb381d6
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-05-08 12:18:30 +0530

    fix indentation
    indentation fix
    
    more indent fix

--- spec/f_spec.rb
+++ spec/f_spec.rb
@@ -3,68 +3,66 @@ require File.expand_path(File.dirname(__FILE__)+"/spec_helper.rb")
 include ExampleWithGSL
 
 describe Distribution::F do
-shared_examples_for "F engine(with rng)" do
-  it "should return correct rng" do
-  pending()
+  shared_examples_for "F engine(with rng)" do
+    it "should return correct rng" do
+      pending()
+    end
   end
-end
 
-shared_examples_for "F engine(with pdf)" do
-  it_only_with_gsl "should return correct pdf" do
-    if @engine.respond_to? :pdf
-      [0.1,0.5,1,2,10,20,30].each{|f|
-      [2,5,10].each{|n2|
-      [2,5,10].each{|n1|
-      @engine.pdf(f,n1,n2).should be_within(1e-4).of(GSL::Ran.fdist_pdf(f,n1,n2))
-      }
-      }
-      }
-    else
-      pending("No #{@engine}.pdf")
+  shared_examples_for "F engine(with pdf)" do
+    it_only_with_gsl "should return correct pdf" do
+      if @engine.respond_to? :pdf
+        [0.1,0.5,1,2,10,20,30].each do |f|
+          [2,5,10].each do |n2|
+            [2,5,10].each do |n1|
+              @engine.pdf(f,n1,n2).should be_within(1e-4).of(
+                GSL::Ran.fdist_pdf(f,n1,n2)
+              )
+            end
+          end
+        end
+      else
+        pending("No #{@engine}.pdf")
+      end
     end
   end
-end
 
-shared_examples_for "F engine" do
-  
-  it_only_with_gsl "should return correct cdf" do
-    if @engine.respond_to? :cdf
-      [0.1,0.5,1,2,10,20,30].each{|f|
-      [2,5,10].each{|n2|
-      [2,5,10].each{|n1|
-      @engine.cdf(f,n1,n2).should be_within(1e-4).of(GSL::Cdf.fdist_P(f,n1,n2))
-      
-      }
-      }
-      }
-    else
-      pending("No #{@engine}.cdf")
+  shared_examples_for "F engine" do
+    it_only_with_gsl "should return correct cdf" do
+      if @engine.respond_to? :cdf
+        [0.1,0.5,1,2,10,20,30].each do |f|
+          [2,5,10].each do |n2|
+            [2,5,10].each do |n1|
+              @engine.cdf(f,n1,n2).should be_within(1e-4).of(GSL::Cdf.fdist_P(f,n1,n2))        
+            end
+          end
+        end
+      else
+        pending("No #{@engine}.cdf")
+      end
     end
-  
-  end
-  it_only_with_gsl "should return correct p_value" do
-    if @engine.respond_to? :p_value
-  
-    [0.1,0.5,1,2,10,20,30].each{|f|
-    [2,5,10].each{|n2|
-    [2,5,10].each{|n1|
-    area=@engine.cdf(f,n1,n2)
-    @engine.p_value(area,n1,n2).should be_within(1e-4).of(GSL::Cdf.fdist_Pinv(area,n1,n2))
-    }
-    }
-    }
-  
-    
-    else
-      pending("No #{@engine}.p_value")
+
+    it_only_with_gsl "should return correct p_value" do
+      if @engine.respond_to? :p_value
+        [0.1,0.5,1,2,10,20,30].each do |f|
+          [2,5,10].each do|n2|
+            [2,5,10].each do |n1|
+              area = @engine.cdf(f,n1,n2)
+              @engine.p_value(area,n1,n2).should be_within(1e-4).of(GSL::Cdf.fdist_Pinv(area,n1,n2))
+            end
+          end
+        end
+      else
+        pending("No #{@engine}.p_value")
+      end
     end
   end
-end
 
   describe "singleton" do
     before do
       @engine=Distribution::F
     end
+
     it_should_behave_like "F engine"    
     it_should_behave_like "F engine(with pdf)"    
   end
@@ -76,6 +74,7 @@ end
     it_should_behave_like "F engine"    
     it_should_behave_like "F engine(with pdf)"    
   end
+
   if Distribution.has_gsl?
     describe Distribution::F::GSL_ do
       before do
@@ -84,7 +83,8 @@ end
     it_should_behave_like "F engine"    
     it_should_behave_like "F engine(with pdf)"    
     end
-  end  
+  end
+
   if Distribution.has_statistics2?
     describe Distribution::F::Statistics2_ do
       before do
@@ -103,5 +103,4 @@ end
     it_should_behave_like "F engine(with pdf)"    
     end  
   end
-  
 end
