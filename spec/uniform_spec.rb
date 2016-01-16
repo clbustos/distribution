ddd3a081d56dd9e6f0afd20c295bd6707266e307
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2016-01-16 15:41:14 +0000

    Specs pending for uniform distribution

--- spec/uniform_spec.rb
+++ spec/uniform_spec.rb
@@ -1,15 +1,51 @@
 require File.expand_path(File.dirname(__FILE__)+"/spec_helper.rb")
-  
+
 describe Distribution::Uniform do
     
-  shared_examples_for "uniform engine" do
+  shared_examples_for "uniform engine with rng" do
+    
+    it "should return correct rng" do
+      samples = 100
+      sum = 0
+      ss = 0
+      lower = 0
+      upper = rand(20)
+      
+      # Expectations
+      exp_mean = (upper + lower) / 2
+      exp_variance = (upper - lower) ** 2 / 12
+      rng = @engine.rng(exp_lower, exp_upper)
+      
+      # Calculate the chi-squared test statistic
+      samples.times do
+        v = rng.call
+        sum += v
+        ss += (v - exp_mean) ** 2
+      end
+      
+      mean = sum.to_f / samples
+      variance = ss.to_f / samples
+      mean.should be_within(1e-5).of(exp_mean)
+      variance.should be_within(1e-5).of(exp_variance)
+    end
+    
+    it "rng with a specified seed should be reproducible" do
+      seed = Random.new_seed
+      rng1 = @engine.rng(0, 1, seed) 
+      rng2 = @engine.rng(0, 1, seed)
+      
+      (rng1.call).should eq(rng2.call)
+    end
+    
+  shared_examples_for "uniform engine with pdf" do
     it "should return correct pdf" do
       if @engine.respond_to? :pdf
+
       else
         pending("No #{@engine}.pdf")
       end
     end
-    
+  end  
     it "should return correct cdf" do
       if @engine.respond_to? :cdf
       else
@@ -21,7 +57,7 @@ describe Distribution::Uniform do
     it "should return correct p_value" do
       if @engine.respond_to? :p_value
       else
-        pending("No #{@engine}.cdf")
+        pending("No #{@engine}.p_value")
       end
     end
   end
